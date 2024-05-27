d2bb6a656b8864780b2aceb82948d64f8373c631
TBradyPW <timothybrady532@gmail.com>
2024-05-27 08:37:04 +0700

    icon update

--- resources/views/layout/about_layout.blade.php
+++ resources/views/layout/about_layout.blade.php
@@ -18,7 +18,7 @@
 
     <!-- Favicon -->
     <link rel="shortcut icon"
-        href="{{ asset('assets/img/Hexagon-logo-NT.png" title="Favicon" sizes="16x16" />
+        href="{{ asset('assets/img/Hexagon-logo-NT.png') }}" title="Favicon" sizes="16x16" />
                 
                     <!-- ====== bootstrap icons cdn ====== --><link rel="stylesheet" href="../cdnjs.cloudflare.com/ajax/libs/bootstrap-icons/1.10.5/font/bootstrap-icons.min.css') }}"
         integrity="sha512-ZnR2wlLbSbr8/c9AgLg3jQPAattCUImNsae6NHYnS9KrIwRdcY9DxFotXhNAKIKbAXlRnujIqUWoXXwqyFOeIQ=="
