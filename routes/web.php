ea227de560716e69f5d2e6b9e5971db6e4cfeb5a
TBradyPW <timothybrady532@gmail.com>
2024-06-13 11:51:32 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -14,10 +14,10 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('Home');
+    return redirect('/home');
 })->name('Home');
 
-Route::get('/Home', function () {
+Route::get('/home', function () {
     return view('Home');
 })->name('Home');
 
