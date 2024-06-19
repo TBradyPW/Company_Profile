1ea76408efee7cf4a69e49ff94d22aae1c5bfaab
TBradyPW <timothybrady532@gmail.com>
2024-06-19 13:37:56 +0700

    dwa
    da

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return redirect('/home');
+    // return redirect('/home');
+    Route::get('/', [App\Http\Controllers\])
 })->name('Home');
 
 Route::get('/home', function () {
